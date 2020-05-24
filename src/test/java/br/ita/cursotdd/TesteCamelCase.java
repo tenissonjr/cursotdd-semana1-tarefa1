cd8e9208fad767093cbb7f55d33f8e9342d8703c
Tenisson Chaves Jr <tenisson.jr@gmail.com>
2020-05-23 22:02:00 -0300

    Novo metodo de teste

--- src/test/java/br/ita/cursotdd/TesteCamelCase.java
+++ src/test/java/br/ita/cursotdd/TesteCamelCase.java
@@ -1,7 +1,24 @@
 package br.ita.cursotdd;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
 public class TesteCamelCase {
 
-
+	@Test
+	public void quandoInformarStringVaziaOuNula_DeveRetornarListaVazia() {
+		
+		List<String> palavras = StringUtil.converterCamelCase(null);
+		
+		assertTrue(palavras!=null && palavras.isEmpty());
+		
+		palavras = StringUtil.converterCamelCase("");
+		
+		assertTrue(palavras!=null && palavras.isEmpty());
+		
+	}
 
 }
