50476dfba58af70539cd668554569ebd47dfce18
P_6677 <tenisson.junior@camara.leg.br>
2020-05-23 22:46:41 -0300

    Alteração da classe para passar no primeiro teste

---
+++ src/main/java/br/ita/cursotdd/StringUtil.java
@@ -0,0 +1,12 @@
+package br.ita.cursotdd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringUtil {
+
+	public static List<String> converterCamelCase(Object object) {
+		return new ArrayList<String>();
+	}
+
+}
