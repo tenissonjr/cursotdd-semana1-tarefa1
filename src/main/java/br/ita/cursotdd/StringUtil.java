6e016f1ae3c6ea6abc49bad64978d569e2161913
P_6677 <tenisson.junior@camara.leg.br>
2020-05-24 22:30:01 -0300

    Correção da classe para passar nos testes

--- src/main/java/br/ita/cursotdd/StringUtil.java
+++ src/main/java/br/ita/cursotdd/StringUtil.java
@@ -3,6 +3,7 @@ package br.ita.cursotdd;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,13 +21,22 @@ public class StringUtil {
 		return valor;
 	}
 
+	private static boolean isValorInvalido(String valorParam) {
+		return ! Pattern.matches("[a-zA-Z]+", valorParam);
+	}
+	
+	
 	public static List<String> converterCamelCase(String valorParam) {
 
 		if (valorParam == null || valorParam.trim().length() == 0) {
 			return new ArrayList<String>();
 		}
+		if (isValorInvalido(valorParam)) {
+			throw new RuntimeException("Inválido-caracteres especiais não são permitidos, somente letras e números.");
+		}
 		String valor = formatarString(valorParam);
 
 		return Stream.of(valor).collect(Collectors.toList());
 	}
+
 }
