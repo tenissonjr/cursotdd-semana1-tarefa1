7dc4b07893ffc2a1385e8587f2ca22cf3f7550ec
P_6677 <tenisson.junior@camara.leg.br>
2020-05-25 10:08:17 -0300

    Refatoração

--- src/main/java/br/ita/cursotdd/StringUtil.java
+++ src/main/java/br/ita/cursotdd/StringUtil.java
@@ -4,75 +4,75 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StringUtil {
 
 	private static Map<String, String> excecoes = Map.of("CPF", "CPF");
 
-	private static String formatarString(String valor) {
 
-		if (excecoes.containsKey(valor)) {
-			valor = excecoes.get(valor);
-		} else {
-			valor = valor.toLowerCase();
-		}
-		return valor;
-	}
-
-	private static String obterPrimeiraPalavraExcecao(String valorParam) {
+	private static boolean isPrimeiraPalavraEhExececao(String valorParam) {
 		for (String palavraExcecao : excecoes.values()) {
 			if(valorParam.trim().startsWith(palavraExcecao)) {
-				return palavraExcecao;
+				return true;
 			}
 		} 
-		return "";
+		return false;
 	}
-	
-	
+
 	private static boolean isValorInvalido(String valorParam) {
 		return ! Pattern.matches("[a-zA-Z]+", valorParam);
 	}
+
+	private static String[] getDivisaoPalavrasExecao(String palavraInicial) {
+		for (String palavraExcecao : excecoes.values()) {
+			if(palavraInicial.trim().startsWith(palavraExcecao)) {
+				String primeiraPalavra  = excecoes.get(palavraExcecao);
+				String demaisPalavras  	= palavraInicial.replace(palavraExcecao, "");
+				return new String[]{primeiraPalavra, demaisPalavras} ;
+			}
+		}		
+		return new String[]{palavraInicial, ""} ;	
+	}
 	
+	private static String[] getDivisaoPalavrasOrdinarias(String palavraInicial) {
+		for (int posicao = 0; posicao < palavraInicial.length(); posicao++) {
+			if(Character.isUpperCase(palavraInicial.charAt(posicao)) && posicao>0) {
+				String primeiraPalavra =  (palavraInicial.substring(0, posicao)).toLowerCase();
+				String demaisPalavras  	= palavraInicial.substring(posicao);
+				return new String[]{primeiraPalavra, demaisPalavras} ;		
+			}
+		}	
+		return new String[]{palavraInicial.toLowerCase(), ""} ;
+	}	
 	
 	public static List<String> converterCamelCase(String valorParam) {
 
+		List<String> retorno = new ArrayList<String>() ;
+		
 		if (valorParam == null || valorParam.trim().length() == 0) {
-			return new ArrayList<String>();
+			return retorno;
 		}
 		if (isValorInvalido(valorParam)) {
 			throw new RuntimeException("Inválido-caracteres especiais não são permitidos, somente letras e números.");
-		}
+		}	
 		
-		String primeiraPalavraExececao = obterPrimeiraPalavraExcecao(valorParam);
-		if (primeiraPalavraExececao!="") {
-			String primeiraPalavra = excecoes.get(primeiraPalavraExececao);
-			List<String> palavras = Stream.of(primeiraPalavra).collect(Collectors.toList());
-			
-			String palavrasRestantes = valorParam.replace(primeiraPalavraExececao, "");
-			palavras.addAll(converterCamelCase(palavrasRestantes))	;
-			
-			return palavras;
-		} else {
-			int posicao = 0 ;
-			for (Character ch : valorParam.toCharArray()) {
-				if(Character.isUpperCase(ch) && posicao>0) {
-						String primeiraPalavra = valorParam.substring(0, posicao);
-						List<String> palavras = converterCamelCase(primeiraPalavra);
-						
-						String palavrasRestantes = valorParam.substring(posicao);
-						palavras.addAll(converterCamelCase(palavrasRestantes))	;
-							
-					return palavras ;
-				}
-				posicao++;
-			}
+		String[] arrDivisaoPalavras = {"",""};
+		if (isPrimeiraPalavraEhExececao(valorParam)) {
+			arrDivisaoPalavras =getDivisaoPalavrasExecao(valorParam);
+		}else {
+			arrDivisaoPalavras =getDivisaoPalavrasOrdinarias(valorParam);
 		}
-		String valor = formatarString(valorParam);
 
-		return Stream.of(valor).collect(Collectors.toList());
+		retorno.add(arrDivisaoPalavras[0]);
+		if (arrDivisaoPalavras[1]!="") {
+			retorno.addAll(converterCamelCase(arrDivisaoPalavras[1])); 
+		}
+		return retorno ;
 	}
+
+
+
+
 
 
 
